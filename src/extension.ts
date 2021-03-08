5840cd03f5c9f4613272f83764b5af3bb07b6bda
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2021-03-07 21:30:57 -0300

    feat: An extension that finds synonyms.

---
+++ src/extension.ts
@@ -0,0 +1,50 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import fetch from 'node-fetch';
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	
+	console.log('Congratulations, your extension "synonym-finder" is now active!');
+
+	let disposable = vscode.commands.registerCommand('synonym-finder.synonymFinder', 
+	async () => {
+		
+
+		const editor = vscode.window.activeTextEditor
+		if(!editor) {
+			vscode.window.showInformationMessage('editor does not exist');
+			return;
+		}
+
+		const text = editor.document.getText(editor.selection)
+
+		const response = await fetch(`https://api.datamuse.com/words?ml=${text.replace(' ', '+')}`)
+		const data = await response.json()
+
+		console.log(text)
+
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.items = data.map((x: any) => ({label: x.word}))
+		quickPick.onDidChangeSelection(([item]) => {
+			if (item) {
+				editor.edit((edit) => {
+					edit.replace(editor.selection, item.label)
+				})
+				quickPick.dispose()
+			}
+		});
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
+
+
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
